963cc4d0b8b9533ad29c8ea57353bd3f47686d20
Nahuel Lucero <nahuel.bm6@gmail.com>
2018-10-31 17:20:18 -0400

    Add no-console lint rule

--- src/server/index.ts
+++ src/server/index.ts
@@ -28,6 +28,7 @@ app.prepare().then(() => {
 
   server.listen(port, err => {
     if (err) throw err
+    /* tslint:disable-next-line no-console */
     console.log(`> Ready on http://localhost:${port}`)
   })
 })
